1fa84fe448ee29bc9cb24dca845bf97fc563c2b5
Kevin C. Baird <kbaird@twistage.com>
2012-11-01 09:37:41 -0700

    Convert credentials and Video GUID to be within Corcoran co on kcb dev box

--- update_via_ar.rb
+++ update_via_ar.rb
@@ -1,13 +1,13 @@
 require './video.rb'
-GUID = '06c25e44f988c'
+GUID = 'bc3f6246c3ed2'
 
 v = Video.find(GUID)
-v.put(:update_single_field, { field: 'title', value: 'myria (before AR update)' })
+v.put(:update_single_field, { field: 'title', value: 'before AR update' })
 v.reload
 puts "Video #{GUID} title is '#{v.title}'"
-v.put(:update_single_field, { field: 'title', value: 'myria-updated via AR' })
+v.put(:update_single_field, { field: 'title', value: 'updated via AR' })
 v.reload
 puts "Video #{GUID} title is now '#{v.title}'"
-v.put(:update_single_field, { field: 'title', value: 'myria-updated after AR' })
+v.put(:update_single_field, { field: 'title', value: 'updated after AR' })
 v.reload
 puts "Video #{GUID} title is now '#{v.title}'"
