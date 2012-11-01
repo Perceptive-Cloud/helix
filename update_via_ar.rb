fb5dae15af744fd2e27e1304c638d7b22476973f
Kevin C. Baird <kbaird@twistage.com>
2012-11-01 10:18:42 -0700

    DRY up updates with loop

--- update_via_ar.rb
+++ update_via_ar.rb
@@ -2,12 +2,8 @@ require './video.rb'
 GUID = '06c25e44f988c'
 
 v = Video.find(GUID)
-v.put(:update_single_field, { field: 'title', value: 'myria (before AR update)' })
-v.reload
-puts "Video #{GUID} title is '#{v.title}'"
-v.put(:update_single_field, { field: 'title', value: 'myria-updated via AR' })
-v.reload
-puts "Video #{GUID} title is now '#{v.title}'"
-v.put(:update_single_field, { field: 'title', value: 'myria-updated after AR' })
-v.reload
-puts "Video #{GUID} title is now '#{v.title}'"
+['before AR', 'updated via AR'].each do |desired_title|
+  v.put(:update_single_field, { field: 'title', value: desired_title })
+  v.reload
+  puts "Video #{GUID} title is '#{v.title}'"
+end
