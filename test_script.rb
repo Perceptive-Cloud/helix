e6cd82e6784d051c17520669abae0a7b58350835
Kevin C. Baird <kbaird@twistage.com>
2012-11-05 15:58:16 -0800

    Minor changes to test_script.rb
    Everything passes for me at the moment

--- test_script.rb
+++ test_script.rb
@@ -3,23 +3,25 @@ require 'helix'
 FILENAME = './helix.yml'
 VIDEO_ID = YAML.load(File.open(FILENAME))['video_id']
 
-{ 'track_id' => Helix::Track, 'video_id' => Helix::Video }.each do |guid_key,klass|
+media_by_id = {
+  'track_id' => Helix::Track,
+  'video_id' => Helix::Video
+}
+media_by_id.each do |guid_key,klass|
+
+  items = klass.find_all(query: 'rest-client', status: :complete)
+  puts "Searching #{klass.to_s} on query => 'rest-client' returns #{items}"
 
   media_id = YAML.load(File.open(FILENAME))[guid_key]
   item = klass.find(media_id)
 
-  if klass == Helix::Video
-    items = klass.find_all(query: 'rest-client')
-    puts "Searching #{klass.to_s} on query => 'rest-client' returns #{items}"
-
-    h = {
-      # these keys are only available on the oobox branch
-      #comments:    item.comments,
-      #ratings:     item.ratings,
-      screenshots: item.screenshots,
-    }
-    puts "#{klass.to_s} #{media_id} has #{h}"
-  end
+  h = {
+    # these keys are only available on the oobox branch
+    #comments:    item.comments,
+    #ratings:     item.ratings,
+    screenshots: item.screenshots,
+  }
+  puts "#{klass.to_s} #{media_id} has #{h}"
 
   ['before rest-client', 'updated via rest-client' ].each do |desired_title|
     item.update(title: desired_title, description: "description of #{desired_title}")
