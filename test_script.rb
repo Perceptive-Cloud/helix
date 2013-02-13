9a97cd3d6c656dcb81a2439ae57775f9bb8c5fb5
Kevin C. Baird <kbaird@twistage.com>
2013-02-13 15:26:54 -0800

    Updates to test_script.rb

--- test_script.rb
+++ test_script.rb
@@ -3,10 +3,10 @@ require 'helix'
 config = Helix::Config.load('./helix.yml')
 
 media_by_id = {
-  'album_id' => Helix::Album,
-  'image_id' => Helix::Image,
-  'track_id' => Helix::Track,
-  'video_id' => Helix::Video
+  album_id: Helix::Album,
+  image_id: Helix::Image,
+  track_id: Helix::Track,
+  video_id: Helix::Video
 }
 media_by_id.each do |guid_key,klass|
 
@@ -18,7 +18,7 @@ media_by_id.each do |guid_key,klass|
   item = klass.find(media_id)
   puts "Read #{klass.to_s} from guid #{media_id}: #{item.inspect}"
 
-  if guid_key == 'video_id'
+  if guid_key == :video_id
     h = {
       # these keys are only available on the oobox branch
       #comments:    item.comments,
@@ -26,9 +26,11 @@ media_by_id.each do |guid_key,klass|
       screenshots: item.screenshots,
     }
     puts "#{klass.to_s} #{media_id} has #{h}"
+    #puts item.download_url
+    #puts item.play
   end
 
-  next if guid_key == 'album_id' # No Update API yet
+  next if guid_key == :album_id # No Update API yet
 
   ['before rest-client', 'updated via rest-client' ].each do |desired_title|
     item.update(title: desired_title, description: "description of #{desired_title}")
@@ -37,10 +39,11 @@ media_by_id.each do |guid_key,klass|
     puts "#{klass.to_s} #{media_id} description is '#{item.description}'"
   end
 
-  media_type  = guid_key.split(/_/).first
+  media_type  = guid_key.to_s.split(/_/).first
   helix_stats = Helix::Statistics
   %w(delivery ingest storage).each do |stats_type|
-    next if %w(album image).include?(media_type) and stats_type == 'ingest'
+    next if [:album_id, :image_id].include?(guid_key) and stats_type == 'ingest'
+    next if [:image_id].include?(guid_key) and stats_type == 'delivery'
     stats = helix_stats.send("#{media_type}_#{stats_type}")
     puts "#{klass.to_s} #{stats_type} stats = #{stats.inspect}"
     item_stats = helix_stats.send("#{media_type}_#{stats_type}", guid_key => media_id)
