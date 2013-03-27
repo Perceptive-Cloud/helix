e42653ed4fa901db960e426524f2c07f0b608e84
Kevin C. Baird <kbaird@twistage.com>
2013-03-27 13:46:51 -0700

    Add TODO notes for better Rectification of names   (http://en.wikipedia.org/wiki/Rectification_of_names)
    Specifically for Media class

--- lib/helix/media.rb
+++ lib/helix/media.rb
@@ -2,6 +2,11 @@ require 'helix/base'
 
 module Helix
 
+  ## TODO: Media is a bad name, as it is also ancestral to Library, which really isn't a media type
+  ## Create a new Mixin called Restful, pull current Media logic into that
+  ## Mix Restful into Media
+  ## Remove Media ancestry from Library
+  ## Mix Restful into Library
   class Media < Base
     # Creates a new record via API and then returns an instance of that record.
     #
