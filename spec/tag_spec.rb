2856978fb285e926cf6a3fc4dadd448ff51f0020
mwood <mwood@twistage.com>
2013-05-07 13:14:38 -0700

    Specs for override of get_data_sets.

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -12,6 +12,27 @@ describe Helix::Tag do
   it { should respond_to(:all)}
   it { should respond_to(:find_all)}
 
+
+  describe ".get_data_sets" do
+    let(:meth)         { :get_data_sets }
+    let(:raw_response) { {"tags" => :expected} }
+    let(:mock_config)  { mock(Helix::Config, build_url: :the_url, get_response: raw_response) }
+    let(:opts)         { {} }
+    before(:each)      { klass.stub(:config) { mock_config }}
+    bu_opts          = {content_type: :xml, resource_label: "tags"}
+    it "should receive build_url(#{bu_opts})" do
+      mock_config.should_receive(:build_url).with(bu_opts) { :the_url }
+      klass.send(meth, opts)
+    end
+    it "should call mock_config.get_response()" do
+      mock_config.should_receive(:get_response).with(:the_url, {sig_type: :view}.merge(opts)) { raw_response }
+      klass.send(meth, opts)
+    end
+    it "should return raw_response[:tags]" do
+      expect(klass.send(meth, opts)).to eq(:expected)
+    end
+  end
+
   describe "Constants"
 
   describe "an instance" do
