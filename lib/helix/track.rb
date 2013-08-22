d450db9eb916df9ccadb4014fad4c1ad2d1ed5e9
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-08-22 10:49:11 -0700

    US26616: Add support for Audio Track Progressive Download API (both file & play actions); Specs

--- lib/helix/track.rb
+++ lib/helix/track.rb
@@ -16,6 +16,31 @@ module Helix
     # @return [Symbol] Name of the class.
     def self.resource_label_sym; :track; end
 
+    # Used to download data for the given Track.
+    #
+    # @example
+    #   track      = Helix::Track.find("239c59483d346")
+    #   track_data = track.download #=> xDC\xF1?\xE9*?\xFF\xD9
+    #   File.open("my_track.mp3", "w") { |f| f.puts track_data }
+    #
+    # @param  [Hash] opts a hash of options for building URL
+    # @return [String] Raw track data, save it to a file
+    def download(opts={})
+      generic_download(opts.merge(action: :file))
+    end
+
+    # Used to play the given Track.
+    #
+    # @example
+    #   track      = Helix::Track.find("239c59483d346")
+    #   track_data = track.play #=> xDC\xF1?\xE9*?\xFF\xD9
+    #
+    # @param  [Hash] opts a hash of options for building URL
+    # @return [String] Raw track data
+    def play(opts={})
+      generic_download(opts.merge(action: :play))
+    end
+
   end
 
 end
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -25,6 +25,69 @@ describe Helix::Track do
     [:destroy, :update].each do |crud_call|
       it { should respond_to(crud_call) }
     end
+
+    describe "#download" do
+      let(:meth)        { :download }
+      let(:mock_config) { double(Helix::Config, build_url: :the_built_url, signature: :some_sig) }
+      subject      { obj.method(meth) }
+      let(:params) { { params: {signature: :some_sig } } }
+      before do
+        obj.stub(:config)            { mock_config }
+        obj.stub(:guid)              { :some_guid  }
+        obj.stub(:plural_resource_label) { :resource_label }
+        RestClient.stub(:get) { '' }
+      end
+      { '' => '', mp3: :mp3, nil => '' }.each do |arg,actual|
+        build_url_h = {action: :file, content_type: actual, guid: :some_guid, resource_label: :resource_label}
+        context "when given {content_type: #{arg}" do
+          it "should build_url(#{build_url_h})" do
+            mock_config.should_receive(:build_url).with(build_url_h)
+            obj.send(meth, content_type: arg)
+          end
+          it "should get a view signature" do
+            mock_config.should_receive(:signature).with(:view) { :some_sig }
+            obj.send(meth, content_type: arg)
+          end
+          it "should return an HTTP get to the built URL with the view sig" do
+            mock_config.stub(:build_url).with(build_url_h) { :the_url }
+            RestClient.should_receive(:get).with(:the_url, params) { :expected }
+            expect(obj.send(meth, content_type: arg)).to be(:expected)
+          end
+        end
+      end
+    end
+
+    describe "#play" do
+      let(:meth)        { :play }
+      let(:mock_config) { double(Helix::Config, build_url: :the_built_url, signature: :some_sig) }
+      subject      { obj.method(meth) }
+      let(:params) { { params: {signature: :some_sig } } }
+      before do
+        obj.stub(:config)            { mock_config }
+        obj.stub(:guid)              { :some_guid  }
+        obj.stub(:plural_resource_label) { :resource_label }
+        RestClient.stub(:get) { '' }
+      end
+      { '' => '', mp3: :mp3, nil => '' }.each do |arg,actual|
+        build_url_h = {action: :play, content_type: actual, guid: :some_guid, resource_label: :resource_label}
+        context "when given {content_type: #{arg}" do
+          it "should build_url(#{build_url_h})" do
+            mock_config.should_receive(:build_url).with(build_url_h)
+            obj.send(meth, content_type: arg)
+          end
+          it "should get a view signature" do
+            mock_config.should_receive(:signature).with(:view) { :some_sig }
+            obj.send(meth, content_type: arg)
+          end
+          it "should return an HTTP get to the built URL with the view sig" do
+            mock_config.stub(:build_url).with(build_url_h) { :the_url }
+            RestClient.should_receive(:get).with(:the_url, params) { :expected }
+            expect(obj.send(meth, content_type: arg)).to be(:expected)
+          end
+        end
+      end
+    end
+
   end
 
   ### CLASS METHODS
