a67fddf03eaeb74113f44e3fbbb81ba7fb8cc29b
Michael Wood <mykewould@gmail.com>
2013-09-12 16:46:54 -0700

    Beginning to move paginates testing into seperate file.

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -336,34 +336,6 @@ describe Helix::Config do
     end
   end
 
-  describe "#get_aggregated_data_sets" do
-    let(:meth)  { :get_aggregated_data_sets }
-    subject     { obj.method(meth) }
-    its(:arity) { should eq(-3) }
-    context "when called" do
-      let(:opts)  { {opts_key1: :opts_val1, per_page: 99} }
-      let(:label) { :videos }
-      before(:each) do
-        obj.stub(:signature) { :the_sig }
-      end
-      subject { obj.send(meth, :a_url, label, opts) }
-      it "should successively call RestClient.get with the opts arg merged with pagination info and return the parsed results" do
-        base_opts = {opts_key1: :opts_val1, per_page: 99, signature: :the_sig}
-        opts1 = {params: base_opts.merge(page: 1)}
-        opts2 = {params: base_opts.merge(page: 2)}
-        opts3 = {params: base_opts.merge(page: 3)}
-        non_final_response = double(String, headers: {is_last_page: 'false'})
-        final_response     = double(String, headers: {is_last_page: 'true'})
-        RestClient.should_receive(:get).with(:a_url, opts1) { non_final_response }
-        RestClient.should_receive(:get).with(:a_url, opts2) { non_final_response }
-        RestClient.should_receive(:get).with(:a_url, opts3) { final_response }
-        obj.stub(:parse_response_by_url_format).with(non_final_response, :a_url) { {label => [:non_final]} }
-        obj.stub(:parse_response_by_url_format).with(final_response, :a_url)     { {label => [:final]}     }
-        expect(obj.send(meth, :a_url, label, opts)).to eq([:non_final, :non_final, :final])
-      end
-    end
-  end
-
   describe "#get_response" do
     let(:meth)  { :get_response }
     subject     { obj.method(meth) }
--- spec/paginates_spec.rb
+++ spec/paginates_spec.rb
@@ -3,7 +3,39 @@ require 'helix'
 
 describe Helix::Paginates do
 
-  let(:klass) { Helix::Paginates }
-  subject     { klass }
+  class DummyClass;end
 
+  before(:each) do
+    @klass = DummyClass.new
+    @klass.extend(Helix::Paginates)
+  end
+
+
+  describe "#get_aggregated_data_sets" do
+    let(:meth)  { :get_aggregated_data_sets }
+    subject     { @klass.method(meth) }
+    its(:arity) { should eq(-3) }
+    context "when called" do
+      let(:opts)  { {opts_key1: :opts_val1, per_page: 99} }
+      let(:label) { :videos }
+      before(:each) do
+        @klass.stub(:signature) { :the_sig }
+      end
+      subject { @klass.send(meth, :a_url, label, opts) }
+      it "should successively call RestClient.get with the opts arg merged with pagination info and return the parsed results" do
+        base_opts = {opts_key1: :opts_val1, per_page: 99, signature: :the_sig}
+        opts1 = {params: base_opts.merge(page: 1)}
+        opts2 = {params: base_opts.merge(page: 2)}
+        opts3 = {params: base_opts.merge(page: 3)}
+        non_final_response = double(String, headers: {is_last_page: 'false'})
+        final_response     = double(String, headers: {is_last_page: 'true'})
+        RestClient.should_receive(:get).with(:a_url, opts1) { non_final_response }
+        RestClient.should_receive(:get).with(:a_url, opts2) { non_final_response }
+        RestClient.should_receive(:get).with(:a_url, opts3) { final_response }
+        @klass.stub(:parse_response_by_url_format).with(non_final_response, :a_url) { {label => [:non_final]} }
+        @klass.stub(:parse_response_by_url_format).with(final_response, :a_url)     { {label => [:final]}     }
+        expect(@klass.send(meth, :a_url, label, opts)).to eq([:non_final, :non_final, :final])
+      end
+    end
+  end
 end
