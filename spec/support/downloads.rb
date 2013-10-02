2aac7e9ef52dba0c147f8226bc13a1725fce2a26
Kevin C. Baird <kcbaird@world.oberlin.edu>
2013-10-02 09:57:35 -0700

    DRY up specs with specs/support/*.rb shared examples

---
+++ spec/support/downloads.rb
@@ -0,0 +1,32 @@
+shared_examples_for "downloads" do |obj|    
+  describe "#download" do
+    let(:meth)        { :download }
+    let(:mock_config) { double(Helix::Config, build_url: :the_built_url, signature: :some_sig) }
+    subject      { obj.method(meth) }
+    let(:params) { { params: {signature: :some_sig } } }
+    before do
+      obj.stub(:config)            { mock_config }
+      obj.stub(:guid)              { :some_guid  }
+      obj.stub(:plural_resource_label) { :resource_label }
+      RestClient.stub(:get) { '' }
+    end
+    { '' => '', mp3: :mp3, nil => '' }.each do |arg,actual|
+      build_url_h = {action: :file, content_type: actual, guid: :some_guid, resource_label: :resource_label}
+      context "when given {content_type: #{arg}" do
+        it "should build_url(#{build_url_h})" do
+          mock_config.should_receive(:build_url).with(build_url_h)
+          obj.send(meth, content_type: arg)
+        end
+        it "should get a view signature" do
+          mock_config.should_receive(:signature).with(:view) { :some_sig }
+          obj.send(meth, content_type: arg)
+        end
+        it "should return an HTTP get to the built URL with the view sig" do
+          mock_config.stub(:build_url).with(build_url_h) { :the_url }
+          RestClient.should_receive(:get).with(:the_url, params) { :expected }
+          expect(obj.send(meth, content_type: arg)).to be(:expected)
+        end
+      end
+    end
+  end
+end
--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -7,8 +7,8 @@ describe Helix::Video do
     { list: { entry: values[:url_params] || {} } }.to_xml(root: :add)
   end
 
-  let(:klass) { Helix::Video }
-  subject     { klass }
+  klass = Helix::Video
+  subject { klass }
   mods = [ Helix::Base, Helix::Durationed, Helix::Media ]
   mods.each { |mod| its(:ancestors) { should include(mod) } }
   its(:guid_name)             { should eq('video_id') }
@@ -23,71 +23,12 @@ describe Helix::Video do
   ### INSTANCE METHODS
 
   describe "an instance" do
-    let(:obj) { klass.new({video_id: 'some_video_guid'}) }
+    obj = klass.new({video_id: 'some_video_guid'})
     subject   { obj }
     its(:resource_label_sym) { should be(:video) }
 
-    describe "#download" do
-      let(:meth)        { :download }
-      let(:mock_config) { double(Helix::Config, build_url: :the_built_url, signature: :some_sig) }
-      subject      { obj.method(meth) }
-      let(:params) { { params: {signature: :some_sig } } }
-      before do
-        obj.stub(:config)            { mock_config }
-        obj.stub(:guid)              { :some_guid  }
-        obj.stub(:plural_resource_label) { :resource_label }
-        RestClient.stub(:get) { '' }
-      end
-      { '' => '', mp3: :mp3, nil => '' }.each do |arg,actual|
-        build_url_h = {action: :file, content_type: actual, guid: :some_guid, resource_label: :resource_label}
-        context "when given {content_type: #{arg}" do
-          it "should build_url(#{build_url_h})" do
-            mock_config.should_receive(:build_url).with(build_url_h)
-            obj.send(meth, content_type: arg)
-          end
-          it "should get a view signature" do
-            mock_config.should_receive(:signature).with(:view) { :some_sig }
-            obj.send(meth, content_type: arg)
-          end
-          it "should return an HTTP get to the built URL with the view sig" do
-            mock_config.stub(:build_url).with(build_url_h) { :the_url }
-            RestClient.should_receive(:get).with(:the_url, params) { :expected }
-            expect(obj.send(meth, content_type: arg)).to be(:expected)
-          end
-        end
-      end
-    end
-
-    describe "#play" do
-      let(:meth)        { :play }
-      let(:mock_config) { double(Helix::Config, build_url: :the_built_url, signature: :some_sig) }
-      subject      { obj.method(meth) }
-      let(:params) { { params: {signature: :some_sig } } }
-      before do
-        obj.stub(:config)            { mock_config }
-        obj.stub(:guid)              { :some_guid  }
-        obj.stub(:plural_resource_label) { :resource_label }
-        RestClient.stub(:get) { '' }
-      end
-      { '' => '', mp3: :mp3, nil => '' }.each do |arg,actual|
-        build_url_h = {action: :play, content_type: actual, guid: :some_guid, resource_label: :resource_label}
-        context "when given {content_type: #{arg}" do
-          it "should build_url(#{build_url_h})" do
-            mock_config.should_receive(:build_url).with(build_url_h)
-            obj.send(meth, content_type: arg)
-          end
-          it "should get a view signature" do
-            mock_config.should_receive(:signature).with(:view) { :some_sig }
-            obj.send(meth, content_type: arg)
-          end
-          it "should return an HTTP get to the built URL with the view sig" do
-            mock_config.stub(:build_url).with(build_url_h) { :the_url }
-            RestClient.should_receive(:get).with(:the_url, params) { :expected }
-            expect(obj.send(meth, content_type: arg)).to be(:expected)
-          end
-        end
-      end
-    end
+    it_behaves_like "downloads", obj
+    it_behaves_like "plays",     obj
 
     describe "#stillframe" do
       let(:meth)        { :stillframe }
